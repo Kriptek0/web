40fe482a7b53ae543e5f4732182b69011e5e5d9c
kriptek0 <megazilot12@gmail.com>
2021-10-15 11:24:21 +0300

    web(15.10.2021)

---
+++ template/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title?></title>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="icon.png" type="image/x-icon" rel="shortcut icon">
+        <script src="js/jquery.min.js" defer></script>
+    </head>
+    <body>
+        <header>
+            <div class="title">Web-программирование</div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index">Главная</a>
+                    </li>
+                    <li>
+                        <a href="login">Вход</a>
+                    </li>
+                    <li>
+                        <a href="registration">Регистрация</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-// echo "Hello<br>Мир";
-$a=5;
-$b=6;
-echo '$a+$b='.($a+$b);
