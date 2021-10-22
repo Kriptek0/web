6c6a9fa6a7b63a213c6a4a3a9ae8960861d5c3a1
kriptek0 <megazilot12@gmail.com>
2021-10-22 11:43:00 +0300

    22.10.2021

--- contents/login.php
+++ contents/login.php
@@ -1,3 +1,20 @@
 <section>
-    Авторизация
+<h1 class="autor">Авторизация</h1>
+    <!--<div class="error" id="error"><?=$info;?></div>-->
+    <form method="post">
+        <div class="reg-wrapper">
+            <div class="string">
+                <label>Логин</label>
+                <input data-desc="Логин" type="text" name="login" required>
+            </div>
+            <div class="string">
+                <label>Пароль</label>
+                <input data-desc="Пароль" type="password" name="password" required>
+            </div>
+            <div class="string buttons">
+                <button>Войти</button>
+                <button onclick="document,location='/web/registration'">Регистрация</button>
+            </div>
+        </div>
+    </form>
 </section>
